651a2416f77a8ea5cb77bbd61d187325d083c2c4
dli <daniel.lindberg1@gmail.com>
2011-07-11 09:53:38 +0200

    Changes

--- src/main/resources/test-data-groups.sql
+++ src/main/resources/test-data-groups.sql
@@ -22,7 +22,6 @@ values ('admin','admin',true);
 insert into users(username, password, enabled) 
 values ('guest','guest',true);
 
-<<<<<<< OURS
 insert into users(username, password, enabled) 
 values ('dalla','dalla',true);
 
@@ -37,13 +36,4 @@ where group_name='Administrators';
 
 insert into group_members(group_id, username) 
 select id,'dalla' from groups 
-=======
---Finally, assign users to groups:
-insert into group_members(group_id, username) 
-select id,'guest' from groups 
-where group_name='Users';
-
-insert into group_members(group_id, username) 
-select id,'admin' from groups 
->>>>>>> THEIRS
 where group_name='Administrators';
